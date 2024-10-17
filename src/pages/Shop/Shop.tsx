078d780223b1496003dfbff6f7018194769c7af9
Isabelle Moura <isabelle.moura.pb@compasso.com.br>
2024-10-17 14:47:14 -0300

    page shop quase pronta

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -9,9 +9,47 @@ import muggo from "../../assets/muggo.png";
 import pingky from "../../assets/pingky.png";
 import potty from "../../assets/potty.png";
 import classes from "./Shop.module.css";
+import seta from "../../assets/seta.png";
+import filterIcon from "../../assets/filterIcon.png";
+import fourballsicon from "../../assets/fourballsicon.png";
+import caixinha from "../../assets/caixinha.png";
+
 function Shop() {
   return (
     <>
+      <div className={classes["banner"]}>
+        <h2 className={classes["title-banner"]}>Shop</h2>
+        <div className={classes["home-shop"]}>
+          <a href="#" className={classes["link-home"]}>
+            Home
+          </a>
+          <img src={seta} className={classes["seta"]} />
+          <p className={classes["second-shop"]}>Shop</p>
+        </div>
+      </div>
+      <div className={classes["container-filter"]}>
+        <div className={classes["filter-icons"]}>
+          <img src={filterIcon} />
+          <p>Filter</p>
+          <img src={fourballsicon} />
+          <img src={caixinha} />
+          <div>
+            <p>Showing 1-16 of 32 results</p>
+          </div>
+        </div>
+
+        <div className={classes["range-results"]}>
+          <p>Show</p>
+          <select name="number" id="number">
+            <option value="valor1">16</option>
+          </select>
+          <p>Short by</p>
+          <select name="string" id="string">
+            Default
+          </select>
+        </div>
+      </div>
+
       <div className={classes["cards"]}>
         <Product
           imageLink={product}
@@ -125,6 +163,28 @@ function Shop() {
           discount="Rp 500.000"
           price=""
         />
+      </div>
+      <div className={classes["pages"]}>
+        <div className={classes["page"]}>
+          <a href="#" className={classes["number-page"]}>
+            1
+          </a>
+        </div>
+        <div className={classes["page"]}>
+          <a href="#" className={classes["number-page"]}>
+            2
+          </a>
+        </div>
+        <div className={classes["page"]}>
+          <a href="#" className={classes["number-page"]}>
+            3
+          </a>
+        </div>
+        <div className={classes["next-page"]}>
+          <a href="#" className={classes["next"]}>
+            Next
+          </a>
+        </div>
       </div>
       <Benefits />
     </>
