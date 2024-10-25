d89adeab0139063087a0cedcd4cc9f607ab73db5
Isabelle Moura <isabelle.moura.pb@compasso.com.br>
2024-10-24 21:40:33 -0300

    create-route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
+import Details, { detailsLoader } from "./pages/Details/Details.tsx";
+import Home from "./pages/Home/Home.tsx";
+import Shop from "./pages/Shop/Shop.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App>
+        <Home />
+      </App>
+    ),
+  },
+  {
+    path: "/shop",
+    element: (
+      <App>
+        <Shop />
+      </App>
+    ),
+  },
+  {
+    path: "/details/:id",
+    element: (
+      <App>
+        <Details />
+      </App>
+    ),
+    loader: detailsLoader,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
